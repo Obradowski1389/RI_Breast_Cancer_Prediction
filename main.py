8a64b0075b21b1aad26bb825099f26e864ec50e0
Obradowski1389 <isomidobradovic@gmail.com>
2023-05-27 15:43:31 +0200

    CNN structure

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+
+
+def load_dataset():
+    raise NotImplementedError
+
+
+def preProcess_data():
+    raise NotImplementedError
+
+def handle_incorrect_values():
+    raise NotImplementedError
+
+
+def build_model():
+    raise NotImplementedError
+
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
