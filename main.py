6aa94d9509ea93fca460ffc0eec44ebd6e0bcbdf
Obradowski1389 <isomidobradovic@gmail.com>
2023-05-27 16:34:19 +0200

    Started work on CNN dataprocessing

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 
 
+global X_train, X_test, Y_train, Y_test, df
+
+
 def load_dataset():
+    global df
+    df = pd.read_csv('./Dataset/breast-cancer-wisconsin.data', names=[
+        'id', 'clumpThickness', 'uniformityOfCellSize', 'uniformityOfCellShape', 'MarginalAdhesion', 
+        'singleEpithelialCellSize', 'bareNuclei', 'blandChromatin', 'normalNucleoli', 'mitoses', 'class'
+    ])
+
+    df.drop('id', axis=1, inplace=True)
+
+    print(df.head)
+
+
+def split_data():
     raise NotImplementedError
 
 
-def preProcess_data():
-    raise NotImplementedError
+def handle_incorrect_values_method1():
+    global df
 
-def handle_incorrect_values():
-    raise NotImplementedError
+    df.replace('?', np.nan, inplace=True)
+
+    # replacement_value = 0 
+    # df.fillna(replacement_value, inplace=True)
+
+    df.dropna(inplace=True)
+
+
+def handle_incorrect_values_method2():
+    global df
+    
+    df.replace('?', np.nan, inplace=True)
+    
+    # Convert columns to numeric type
+    df = df.apply(pd.to_numeric, errors='coerce')
+    
+    column_averages = df.mean()
+
+    df.fillna(column_averages, inplace=True)
 
 
 def build_model():
@@ -20,7 +53,8 @@ def build_model():
 
 
 def main():
-    raise NotImplementedError
+    load_dataset()
+    handle_incorrect_values_method1()
 
 
 if __name__ == "__main__":
